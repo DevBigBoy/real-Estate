e0aa8f155521bf9d21f33774d7b90be2279f123a
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-01-03 03:33:01 +0200

    moving all data into admin folder

---
+++ admin/layouts/footer.php
@@ -0,0 +1,9 @@
+</div>
+</div>
+
+<script src="<?php echo BASE_URL; ?>dist-admin/js/scripts.js"></script>
+<script src="<?php echo BASE_URL; ?>dist-admin/js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+
+$dbhost = 'localhost';
+$dbname = 'admin_panel_php';
+$dbuser = 'root';
+$dbpass = '';
+
+try {
+    $pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    echo "Connection error :" . $exception->getMessage();
+}
+
+# 
+define("BASE_URL", "http://localhost/real-estate/");
+define("ADMIN_URL", BASE_URL . "admin/");
+
+#
+define("SMTP_HOST", "sandbox.smtp.mailtrap.io");
+define("SMTP_PORT", "587");
+define("SMTP_USERNAME", "07fe0ea2121097");
+define("SMTP_PASSWORD", "253ba1d6e8ef46");
+define("SMTP_ENCRYPTION", "tls");
+define("SMTP_FROM", "contact@yourwebsite.com");
